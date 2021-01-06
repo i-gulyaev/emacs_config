3dd036e7bcb859c7bdeac4dd10c703c6ba2ff564
Igor Guliaev <igor.s.gulyaev@gmail.com>
2021-01-06 19:36:10 +0300

    Cleanup

--- init.el
+++ init.el
@@ -10,7 +10,6 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
